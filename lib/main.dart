dcf45b0d193824aa8648f5662d588ec34be9e91c
José L. Flores <98982469+JiuseppeFlores@users.noreply.github.com>
2022-10-19 19:59:17 -0400

    "Proyecto Base"

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:cintitur_app/splash.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      debugShowCheckedModeBanner: false,
+      home: Splash(),
+    );
+  }
+}
